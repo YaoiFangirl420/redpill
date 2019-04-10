3323c2670f0664516941369109e7687ec8e6bdb7
YaoiFangirl420 <48789208+YaoiFangirl420@users.noreply.github.com>
2019-04-10 02:59:23 -0700

    Responding to text input

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::io;
 use std::io::stdin;
 use std::thread;
 use std::time::Duration;
+use std::sync::mpsc;
 
 use termion::raw::IntoRawMode;
 use termion::input::TermRead;
@@ -66,17 +67,37 @@ fn main() -> Result<(), io::Error> {
   let backend: TermionBackend<termion::raw::RawTerminal<std::io::Stdout>> = TermionBackend::new(stdout);
   let mut terminal = Terminal::new(backend)?;
 
-  draw_main_ui(&mut terminal, vec![format!("aye"), format!("no")]);
+  let (keys_write, keys_read) = mpsc::channel();
 
+  let stdin_thread = thread::spawn(move || {
     for c in stdin.events() {
       let evt = c.unwrap();
       match evt {
-        Event::Key(Key::Ctrl('q')) => break,
-        Event::Key(Key::Char(c)) => {
+        Event::Key(key) => keys_write.send(key).unwrap(),
+        _ => (),
+      }
+    }
+  });
 
+
+  let mut input_buf = String::new();
+  draw_main_ui(&mut terminal, vec![format!("aye")]);
+  loop {
+    if let Ok(key) = keys_read.try_recv() {
+      match key {
+        Key::Ctrl('q') => break,
+        Key::Char('\n') => {
+          input_buf.clear();
+          draw_main_ui(&mut terminal, vec![format!("aye"), format!("{}", input_buf)]);
+        },
+        Key::Char(c) => {
+          input_buf.push(c);
+          draw_main_ui(&mut terminal, vec![format!("aye"), format!("{}", input_buf)]);
         },
         _ => (),
       }
     }
-    Ok(())
+  }
+
+  Ok(())
 }
